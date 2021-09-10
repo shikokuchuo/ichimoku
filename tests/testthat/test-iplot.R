0664bff616583aa5f3db70e60bb38939ee34160e
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2021-09-10 16:32:30 +0100

    Build 1.1.5.1 development

--- tests/testthat/test-iplot.R
+++ tests/testthat/test-iplot.R
@@ -2,10 +2,12 @@ test_that("iplot Shiny functions ok", {
   skip_if_not_installed("shiny")
   cloud <- ichimoku(sample_ohlc_data, ticker = "TKR", periods = c(9, 26, 52))
   strat <- strat(cloud)
-  expect_s3_class(iplot(strat, theme = "dark"), "shiny.appobj")
+  expect_s3_class(iplot(strat, theme = "dark", type = "bar", custom = "ret"), "shiny.appobj")
   expect_error(iplot(sample_ohlc_data), regexp = "ichimoku object")
   shiny::testServer(iplot(cloud), {
-    session$setInputs(plot_hover = list(x = 2, y = 125, coords_css = list(x = 200, y = 200)),
+    session$setInputs(type = "none", plot_hover = list(x = 2, y = 125, coords_css = list(x = 200, y = 200)),
+                      infotip = TRUE)
+    session$setInputs(type = "bar", custom = "close", plot_hover = list(x = 2, y = 125, coords_css = list(x = 200, y = 200)),
                       infotip = TRUE)
     expect_s3_class(pdata(), "ichimoku")
   })
